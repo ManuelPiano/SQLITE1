eeddd922a8e3730911d3fee884a483905719e484
Manuel Alvarado <101200587+ManuelPiano@users.noreply.github.com>
2022-09-04 23:07:58 -0600

    actualizacion
    subir

---
+++ app/src/main/java/com/primera/sqlite/detalles_articulos.java
@@ -0,0 +1,14 @@
+package com.primera.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalles_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_articulos);
+    }
+}
